6afd7868259d1debe9d008cae1a2063c1d792d16
Your Name <you@example.com>
2023-03-10 00:17:22 -0800

    dependencies added.

---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      {/* Navigation component goes here */}
+    </header>
+  );
+}
+
+export default Header;
